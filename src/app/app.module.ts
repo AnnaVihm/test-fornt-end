5c47ae2c6fcb4f59fab55acd3511b606a2836de0
vitoria <annavitoria00023@gmail.com>
2019-11-19 17:07:31 -0300

    Implementado modal-adicionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuComponent } from './menu/menu.component';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { ModalAdicionarComponent } from './dashboard/modal-adicionar/modal-adicionar.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DashboardComponent
+    DashboardComponent,
+    ModalAdicionarComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
