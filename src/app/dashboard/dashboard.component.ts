c9f8ded6a0fa37936a2d53591dad0c9495860807
vitoria <annavitoria00023@gmail.com>
2019-11-19 15:35:58 -0300

    Melhorias em dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,15 +13,15 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.redesSociais = [
-      { nome: "Facebook", img: '../assets/imgs/icons/facebook.png' },
-      { nome: "Instagram", img: '../assets/imgs/icons/instagram.png' },
-      { nome: "Twitter", img: '../assets/imgs/icons/twitter-logo.png' },
-      { nome: "Google Meu Negócio", img: '../assets/imgs/icons/google-meu-negocio.png' },
-      { nome: "Pinterest", img: '../assets/imgs/icons/pinterest.png' },
-      {nome:"Linkedin", img:'../assets/imgs/icons/linkedin.png'},
-      {nome:"Youtube", img:'../assets/imgs/icons/youtube.png'},
-      {nome:"WhatsApp", img:"../assets/imgs/icons/whastapp.png"},
-      {nome:"Google Analytics", img:"../assets/imgs/icons/analytics.png"}
+      { id: 1, nome: "Facebook", img: '../assets/imgs/icons/facebook.png' },
+      { id: 2, nome: "Twitter", img: '../assets/imgs/icons/twitter-logo.png' },
+      { id: 3, nome: "Instagram", img: '../assets/imgs/icons/instagram.png' },
+      { id: 4, nome: "Google Meu Negócio", img: '../assets/imgs/icons/google-meu-negocio.png' },
+      { id: 5, nome: "Pinterest", img: '../assets/imgs/icons/pinterest.png' },
+      { id: 6, nome: "Linkedin", img: '../assets/imgs/icons/linkedin.png' },
+      { id: 7, nome: "Youtube", img: '../assets/imgs/icons/youtube.png' },
+      { id: 8, nome: "WhatsApp", img: "../assets/imgs/icons/whastapp.png" },
+      { id: 9, nome: "Google Analytics", img: "../assets/imgs/icons/analytics.png" }
     ]
   }
 
